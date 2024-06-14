2ec0039870a2374b28eb101d3b688bbf2d3bf1a3
Pieter Spealman <32845376+pspealman@users.noreply.github.com>
2024-06-13 20:40:57 -0400

    Update module_load.sh
    added mafft, emboss, miniasm

--- demo/module_load.sh
+++ demo/module_load.sh
@@ -3,3 +3,6 @@ module load sniffles/1.0.12
 module load samtools/intel/1.14
 module load bedtools/intel/2.29.2
 module load minimap2/2.22
+module load mafft/intel/7.475
+module load emboss/intel/6.6.0
+module load miniasm/0.3
